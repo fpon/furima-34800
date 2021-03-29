8769cbd8f92be98dc6353a1ebc1a673367dee5cb
fpon <happy193fuga@mineo.jp>
2021-03-29 11:07:40 +0900

    商品購入のテストコード

---
+++ spec/factories/item_purchases.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :item_purchase do
+    post { '123-4567'}
+    region_id { 9 }
+    city { '山田市' }
+    house_number { '1-1' }
+    building_name { '東京ハイツ' }
+    phone_number { '01234567890' }
+    
+    association :user
+    association :item
+  end
+end
