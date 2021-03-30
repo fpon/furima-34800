8a436aded4c88b41f4a78b22e2e84954a1ebdd80
fpon <happy193fuga@mineo.jp>
2021-03-30 15:42:55 +0900

    コード修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -21,7 +21,7 @@ class Item < ApplicationRecord
     validates :day_id
   end
 
-  validates :price, numericality: { greater_than: 300, less_than: 9_999_999 }
+  validates :price, numericality: { greater_than: 299, less_than: 10_000_000 }
   validates :price, format: { with: /\A[0-9]+\z/ }
 
   belongs_to :user
